0ff64dbc9e8cf8437e640222744fb5735ee8cfba
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2016-10-02 18:22:00 -0300

    relase 2.0.0

--- tests/test_version.py
+++ tests/test_version.py
@@ -7,10 +7,10 @@ from inhoj.version import Version
 class TestVersion(unittest.TestCase):
 
     def test_set_version(self):
-        ver = Version("1.0.1")
-        self.assertEqual(ver.number, "1.0.1")
+        ver = Version("1.0.3")
+        self.assertEqual(ver.number, "1.0.3")
 
     def test_version_immutable(self):
-        ver = Version("1.0.1")
+        ver = Version("1.0.3")
         with pytest.raises(TypeError) as e:
-            ver.number = "1.1.1"
+            ver.number = "1.1.3"
